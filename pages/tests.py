699a50abfc173e6873fe7827b036af1ddac115a4
Twinkle <twinkle@MacBook-Pro.local>
2022-03-03 21:30:00 +0530

    Added tests for Pages App

--- pages/tests.py
+++ pages/tests.py
@@ -1,18 +1,29 @@
+from urllib import response
 from django.test import SimpleTestCase
-from django.urls import reverse
+from django.urls import reverse, resolve
+
+from .views import HomePageView
 
 
 class HomePageTests(SimpleTestCase):
 
-    def test_home_page_url(self):
+    def test_homepage_status_code(self):
         response = self.client.get("/")
         self.assertEqual(response.status_code, 200)
 
-    def test_home_reverse_url(self):
+    def test_homepage_url_name(self):
         response = self.client.get(reverse("home"))
         self.assertEqual(response.status_code, 200)
 
-    def test_home_template(self):
+    def test_homepage_template(self):
         response = self.client.get(reverse("home"))
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, "home.html")
+
+    def test_homepage_contains_correct_html(self):
+        response = self.client.get("/")
+        self.assertContains(response, "Home Page")
+
+    def test_homepage_does_not_contain_incorrect_html(self):
+        response = self.client.get("/")
+        self.assertNotContains(response, "Some Dummy Text.")
