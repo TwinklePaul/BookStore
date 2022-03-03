cf77cd5cd3bebaa4fcc5be7c214f12adb9aa5f60
Twinkle <twinkle@MacBook-Pro.local>
2022-03-03 21:22:37 +0530

    Added tests for Pages App

--- pages/tests.py
+++ pages/tests.py
@@ -1,3 +1,18 @@
-from django.test import TestCase
+from django.test import SimpleTestCase
+from django.urls import reverse
 
-# Create your tests here.
+
+class HomePageTests(SimpleTestCase):
+
+    def test_home_page_url(self):
+        response = self.client.get("/")
+        self.assertEqual(response.status_code, 200)
+
+    def test_home_reverse_url(self):
+        response = self.client.get(reverse("home"))
+        self.assertEqual(response.status_code, 200)
+
+    def test_home_template(self):
+        response = self.client.get(reverse("home"))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "home.html")
