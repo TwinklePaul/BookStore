cb9854a908719295a77a158b8984a7d0060e0971
Twinkle <twinkle@MacBook-Pro.local>
2022-03-03 21:39:11 +0530

    Added tests for Pages App

--- pages/tests.py
+++ pages/tests.py
@@ -7,23 +7,32 @@ from .views import HomePageView
 
 class HomePageTests(SimpleTestCase):
 
-    def test_homepage_status_code(self):
-        response = self.client.get("/")
-        self.assertEqual(response.status_code, 200)
+    def setUp(self):
+        self.response = self.client.get(reverse("home"))
 
-    def test_homepage_url_name(self):
-        response = self.client.get(reverse("home"))
-        self.assertEqual(response.status_code, 200)
+    def test_homepage_status_code(self):
+        # response = self.client.get("/")
+        self.assertEqual(self.response.status_code, 200)
+
+    # def test_homepage_url_name(self):
+    #     response = self.client.get(reverse("home"))
+    #     self.assertEqual(response.status_code, 200)
 
     def test_homepage_template(self):
-        response = self.client.get(reverse("home"))
-        self.assertEqual(response.status_code, 200)
-        self.assertTemplateUsed(response, "home.html")
+        # response = self.client.get(reverse("home"))
+        self.assertTemplateUsed(self.response, "home.html")
 
     def test_homepage_contains_correct_html(self):
-        response = self.client.get("/")
-        self.assertContains(response, "Home Page")
+        # response = self.client.get("/")
+        self.assertContains(self.response, "Home Page")
 
     def test_homepage_does_not_contain_incorrect_html(self):
-        response = self.client.get("/")
-        self.assertNotContains(response, "Some Dummy Text.")
+        # response = self.client.get("/")
+        self.assertNotContains(self.response, "Some Dummy Text.")
+
+    def test_homepage_url_resolves_homepageview(self):
+        view = resolve("/")
+        self.assertEqual(
+            view.func.__name__,
+            HomePageView.as_view().__name__
+        )
