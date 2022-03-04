8f446114a8c73bbafcf6e31895f634cb75dbcdc4
Twinkle <twinkle@MacBook-Pro.local>
2022-03-05 03:50:28 +0530

    Removed StaticFiles

--- bookstore_project/settings.py
+++ bookstore_project/settings.py
@@ -23,6 +23,7 @@ ALLOWED_HOSTS = ['*']
 INSTALLED_APPS = [
     'users.apps.UsersConfig',
     'pages.apps.PagesConfig',
+    'books.apps.BooksConfig',
 
     'crispy_forms',
     'crispy_bootstrap5',
