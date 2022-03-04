d22df327574f267273cc63736ea334b5e3665a9b
Twinkle <twinkle@MacBook-Pro.local>
2022-03-04 21:21:36 +0530

    Minor Changes

--- bookstore_project/settings.py
+++ bookstore_project/settings.py
@@ -1,6 +1,6 @@
 from pathlib import Path
 import os
-from telnetlib import AUTHENTICATION
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -10,7 +10,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-9nb)70*2hxx$y^6u-g&oca42xketudp00nrym@cqy2%6^i334)'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -81,9 +81,9 @@ WSGI_APPLICATION = 'bookstore_project.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'bookstore',
-        'USER': 'bookstore_admin',
-        'PASSWORD': 'postgres',
+        'NAME': config('DB_NAME'),
+        'USER': config('DB_USER'),
+        'PASSWORD': config('DB_PASSWORD'),
         'HOST': 'db',
         'PORT': 5432
     }
